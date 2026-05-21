4c0600488fe75b679e6da326f10d0b64587e2234
Matthew Reardon <patriotnewsactivism@proton.me>
2026-05-21 04:35:06 -0500

    feat: register parallel agent routes

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { registerRoutes } from "./routes";
+import { registerParallelRoutes } from "./parallelRoutes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
@@ -40,6 +41,7 @@ app.use((req, res, next) => {
 
 (async () => {
   await registerRoutes(app);
+  registerParallelRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
